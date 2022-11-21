06ea4de9a81bb9492381e2325f21b1ddaa2f897d
Josh Runyan <joshrunyan7@gmail.com>
2022-11-21 10:16:49 -0700

    first

---
+++ functions.js
@@ -0,0 +1,21 @@
+
+var yes = document.getElementById("yes");
+yes.onclick = toggle();
+
+
+
+function toggle() {
+
+  }
+
+function putToast(){
+
+
+}
+
+function stylisticChange(){
+    var textBox = document.getElementById("joe");
+    textBox.style.backgroundColor = "red"
+
+
+}
