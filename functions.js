326024a0ed9c8303e180a6a91efdeadc5d811b30
Josh Runyan <joshrunyan7@gmail.com>
2022-11-21 10:27:15 -0700

    end

--- functions.js
+++ functions.js
@@ -1,11 +1,27 @@
 
-var yes = document.getElementById("yes");
-yes.onclick = toggle();
+  function toggleOptions() {
+    // get the clock
+    var option = document.getElementById('size');
 
+    // get the current value of the clock's display property
+    var displaySetting = myClock.style.display;
 
+    // also get the clock button, so we can change what it says
+    var clockButton = document.getElementById('yes');
 
-function toggle() {
-
+    // now toggle the clock and the button text, depending on current state
+    if (displaySetting == 'block') {
+      // clock is visible. hide it
+      myClock.style.display = 'none';
+      // change button text
+      clockButton.innerHTML = 'Show clock';
+    }
+    else {
+      // clock is hidden. show it
+      myClock.style.display = 'block';
+      // change button text
+      clockButton.innerHTML = 'Hide clock';
+    }
   }
 
 function putToast(){
